74e2bd72dd9fb0374d8b9a0d54fb6c736f7bd152
CaptnJayce <cjest.socials@gmail.com>
2024-09-29 15:54:48 +0100

    music test

--- Globals/Signals.gd
+++ Globals/Signals.gd
@@ -1,1 +1,3 @@
 extends Node
+
+# unused atm
--- Sounds/AudioManager.gd
+++ Sounds/AudioManager.gd
@@ -1,6 +1,13 @@
 extends Node
 
 @onready var clickSound: AudioStreamPlayer = $click
+@onready var musicTest: AudioStreamPlayer = $testMusic
+
+func _ready():
+	playMusic()
 
 func playClick():
 	clickSound.play()
+
+func playMusic():
+	musicTest.play()
